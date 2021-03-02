40f3d8eeec5f1f6647198a3bf28697e49234a0da
Aleksey Uymanov <s9gf4ult@gmail.com>
2021-03-02 22:01:01 +0500

    reworking trees

--- src/OpenAPI/Checker/Report.hs
+++ src/OpenAPI/Checker/Report.hs
@@ -4,6 +4,7 @@ import           Data.Map.Strict                as M
 import           Data.Monoid.Generic
 import           Data.OpenApi.Internal
 import           Data.Semigroup.Generic
+import           Data.Sequence                  (Seq)
 import           Data.Text                      (Text)
 import           GHC.Generics                   (Generic)
 import           OpenAPI.Checker.Validate.Monad
@@ -22,49 +23,49 @@ data Status = Success | Fail Text
 type Path = FilePath -- From the library
 
 newtype ReportTree = ReportTree
-  { paths :: Map Path (Errorable PathItemTree)
+  { paths :: Seq (Diff PathItemTree)
   } deriving (Eq, Ord, Show, Generic, Semigroup, Monoid)
 
-data PathItemTree = PathItemTree
-  { operations :: Map OperationName (Errorable OperationTree)
-  , server     :: Maybe (Errorable ServerTree)
-  } deriving (Eq, Ord, Show, Generic)
+newtype PathItemTree = PathItemTree
+  { operations :: Seq (Diff OperationTree)
+  } deriving (Eq, Ord, Show, Generic, Semigroup, Monoid)
 
-instance Semigroup PathItemTree where
-  (<>) = genericMappend
-
-instance Monoid PathItemTree where
-  mempty = genericMempty
-
-instance Nested PathItemTree where
+instance Node PathItemTree where
   type Parent PathItemTree = ReportTree
-  type Key PathItemTree = Path
-  nest key p = ReportTree $ M.singleton key p
+  type Key PathItemTree = FilePath
+  type Original PathItemTree = PathItem
+  nest sd = ReportTree sd
+
+newtype OperationTree = OperationTree
+  { parameters :: Seq (Diff ParamTree)
+  } deriving (Eq, Ord, Show, Generic, Semigroup, Monoid)
+
+instance Node OperationTree where
+  type Parent OperationTree = PathItemTree
+  type Key OperationTree = OperationName
+  type Original OperationTree = Operation
+  nest sd = PathItemTree sd
 
 data OperationName
   = Get | Put | Post | Delete | Options | Head | Patch | Trace
   deriving (Eq, Ord, Show, Generic)
 
-newtype OperationTree = OperationTree
-  { parameters :: Map ParamKey (Errorable ParamTree)
-  } deriving (Eq, Ord, Show, Generic, Semigroup, Monoid)
-
-instance Nested OperationTree where
-  type Parent OperationTree = PathItemTree
-  type Key OperationTree = OperationName
-  nest key p = PathItemTree (M.singleton key p) mempty
-
 data ServerTree = ServerTree
   deriving (Eq, Ord, Show, Generic)
 
 data ParamTree = ParamTree
-  deriving (Eq, Ord, Show, Generic)
+  { required        :: Errorable ParamRequiredTree
+  , allowEmptyValue :: Errorable ParamAllowEmptyValueTree
+  , allowReserved   :: Errorable ParamAllowReservedTree
+  , schema          :: Maybe (Errorable ParamSchemaTree)
+  , style           :: Errorable ParamStyleTree
+  } deriving (Eq, Ord, Show, Generic)
 
 instance Semigroup ParamTree where
-  (<>) = error "Not implemented"
+  (<>) = genericMappend
 instance Monoid ParamTree where
   mappend = (<>)
-  mempty = error "Not implemented"
+  mempty = genericMempty
 
 instance Nested ParamTree where
   type Key ParamTree = ParamKey
@@ -72,6 +73,21 @@ instance Nested ParamTree where
   nest key p = OperationTree $ M.singleton key p
 
 deriving instance Ord ParamLocation
+
+data ParamRequiredTree = ParamRequiredTree
+  deriving (Eq, Ord, Show, Generic)
+
+data ParamAllowEmptyValueTree = ParamAllowEmptyValueTree
+  deriving (Eq, Ord, Show, Generic)
+
+data ParamAllowReservedTree = ParamAllowReservedTree
+  deriving (Eq, Ord, Show, Generic)
+
+data ParamSchemaTree = ParamSchemaTree
+  deriving (Eq, Ord, Show, Generic)
+
+data ParamStyleTree = ParamStyleTree
+  deriving (Eq, Ord, Show, Generic)
 
 data ParamKey = ParamKey
   { name    :: Text
--- src/OpenAPI/Checker/Validate/Monad.hs
+++ src/OpenAPI/Checker/Validate/Monad.hs
@@ -7,12 +7,15 @@ import           Control.Monad.Trans.Reader
 import           Control.Monad.Trans.State.Strict
 import           Data.Foldable
 import           Data.Functor.Identity
+import           Data.Generics.Product
+import           Data.Monoid.Generic
 import           Data.OpenApi.Internal
+import           Data.Sequence                    (Seq)
 import           Data.Text                        (Text)
 import           Data.Traversable
 import           GHC.Generics                     (Generic)
 
-type TreeM t = ReaderT Env (StateT t Errorable)
+type TreeM n = ReaderT Env (State (Seq (Diff n)))
 
 data Env = Env
   { oldServers :: [Server]
@@ -22,31 +25,66 @@ data Env = Env
 emptyEnv :: Env
 emptyEnv = Env [] []
 
--- | Might be different type in future
-type Errorable = Either Err
+-- | Any diff may be compatible or incompatible, so we split this information
+-- into two different fields.
+data Diff n = Diff
+  { compat :: Compatible
+  , diff   :: DiffDesc n
+  } deriving (Generic)
 
-type Err = Text
+deriving instance (Eq (DiffDesc n)) => Eq (Diff n)
+deriving instance (Ord (DiffDesc n)) => Ord (Diff n)
+deriving instance (Show (DiffDesc n)) => Show (Diff n)
 
--- | Class of trees nested into another trees
-class (Monoid (Parent t)) => Nested t where
-  type Parent t
-  type Key t
-  nest :: Key t -> Errorable t -> Parent t
+data Compatible = Compatible | Uncompatible
+  deriving (Eq, Ord, Show, Generic)
 
-runTreeM :: (Monoid t) => Env -> TreeM t a -> Errorable (a, t)
-runTreeM env ma = runStateT (runReaderT ma env) mempty
+data DiffDesc n
+  = Removed Text (Original n)
+  -- ^ Entity removed in new version. We don't need the key here, because the
+  -- path exists only in one of compared trees
+  | Added Text (Original n)
+  -- ^ Entity added in new version. We don't need the key here, because the path
+  -- exists only in one of compared trees
+  | Changed (Key n) n
+  -- ^ We need the key to determine the path in the tree we are going to.
+  deriving (Generic)
 
--- | Throws error in current tree
-treeError :: Err -> TreeM t a
-treeError = throwError
+deriving instance (Eq n, Eq (Original n), Eq (Key n)) => Eq (DiffDesc n)
+deriving instance (Ord n, Ord (Original n), Ord (Key n)) => Ord (DiffDesc n)
+deriving instance (Show n, Show (Original n), Show (Key n)) => Show (DiffDesc n)
+
+-- | Nodes having parent Tree element
+class (Monoid (Parent n)) => Node n where
+  type Parent n :: *
+  type Key n :: *
+  type Original n :: *
+  nest :: Seq (Diff n) -> Parent n
+
+runTreeM :: Env -> TreeM n a -> (a, Seq (Diff n))
+runTreeM env ma = runState (runReaderT ma env) mempty
+
+diffCompat :: DiffDesc n -> TreeM n ()
+diffCompat desc = do
+  let dif = pure $ Diff Compatible desc
+  id <>= dif
+
+diffUncompat :: DiffDesc n -> TreeM n ()
+diffUncompat desc = do
+  let dif = pure $ Diff Uncompatible desc
+  id <>= dif
+
 
 -- | Runs several computations in different paths in subtrees
 follow
-  :: (Foldable f, Nested t, Monoid t)
+  :: (Traversable f, Node t, Monoid t)
   => f (Key t, TreeM t a)
-  -> TreeM (Parent t) ()
+  -> TreeM (Parent t) (f a)
 follow tree = do
   env <- ask
-  for_ tree $ \(key, sub) -> do
-    let p = nest key $ fmap snd $ runTreeM env sub
+  for tree $ \(key, sub) -> do
+    let
+      (res, seq) = runTreeM env sub
+      p = nest seq
     id <>= p
+    return res
