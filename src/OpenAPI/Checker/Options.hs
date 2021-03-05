b41cc12dd6ef835bf718115374aa0fc5c204ef27
iko <ilyakooo0@gmail.com>
2021-03-05 17:11:17 +0300

    Added basic options

---
+++ src/OpenAPI/Checker/Options.hs
@@ -0,0 +1,61 @@
+module OpenAPI.Checker.Options
+  ( Options (..),
+    Mode (..),
+    optionsParserInfo,
+    execParser,
+  )
+where
+
+import GHC.Generics (Generic)
+import Options.Applicative
+
+data Options = Options
+  { clientFile :: FilePath,
+    serverFile :: FilePath,
+    mode :: Mode
+  }
+  deriving stock (Generic)
+
+data Mode = Silent | OnlyErrors | All
+
+optionsParserInfo :: ParserInfo Options
+optionsParserInfo =
+  info
+    (helper <*> optionsParser)
+    ( fullDesc
+        <> header "openapi-diff"
+        <> progDesc "A tool to check compatibility between two OpenApi specifications."
+    )
+
+optionsParser :: Parser Options
+optionsParser =
+  Options
+    <$> strOption
+      ( short 'c'
+          <> long "client"
+          <> help "The specification that will be used for the client of the API."
+      )
+    <*> strOption
+      ( short 's'
+          <> long "server"
+          <> help "The specification that will be used for the server of the API."
+      )
+    <*> ( flag'
+            Silent
+            ( long "silent"
+                <> help "Silence all output."
+            )
+            <|> flag'
+              OnlyErrors
+              ( long "only-errors"
+                  <> help "Only report incompatibility errors in the output."
+              )
+            <|> flag'
+              All
+              ( long "all"
+                  <> help
+                    "Report both incompatible and compatible changes. \
+                    \Compatible changes will not trigger a failure exit code."
+              )
+            <|> pure All
+        )
