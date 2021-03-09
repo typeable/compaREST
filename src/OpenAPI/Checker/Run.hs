16607664c3204e50abc01240e151f8bd277073fc
iko <ilyakooo0@gmail.com>
2021-03-09 17:06:01 +0300

    Added basic options (#13)

--- src/OpenAPI/Checker/Run.hs
+++ src/OpenAPI/Checker/Run.hs
@@ -1,25 +1,20 @@
 module OpenAPI.Checker.Run (runChecker) where
 
-import           Data.Aeson
-import           Data.OpenApi
-import           OpenAPI.Checker.Report
-import           OpenAPI.Checker.Validate
-
+import Data.Aeson
+import OpenAPI.Checker.Options
+import OpenAPI.Checker.Report
+import OpenAPI.Checker.Validate
 
 runChecker :: IO ()
 runChecker = do
-  let
-    schemaA = (error "FIXME: not implemented")
-    schemaB = (error "FIXME: not implemented")
-  a <- eitherDecodeFileStrict schemaA >>= \case
-    Left e -> do
-      putStrLn $ "Failed to parse: " ++ schemaA
-      fail e
-    Right s -> pure s
-  b <- eitherDecodeFileStrict schemaB >>= \case
-    Left e -> do
-      putStrLn $ "Failed to parse: " ++ schemaB
-      fail e
-    Right s -> pure s
+  opts <- execParser optionsParserInfo
+  let parseSchema path =
+        eitherDecodeFileStrict path >>= \case
+          Left e -> do
+            putStrLn $ "Failed to parse: " ++ path
+            fail e
+          Right s -> pure s
+  a <- parseSchema (clientFile opts)
+  b <- parseSchema (serverFile opts)
   let report = reportCompat a b
   printReport report
