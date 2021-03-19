2e45d1fc6fe8657fb508adfb28624a3989e11641
iko <ilyakooo0@gmail.com>
2021-03-19 14:56:02 +0300

    Removed unused modules

--- src/OpenAPI/Checker/Aux.hs
+++
@@ -1,7 +0,0 @@
-module OpenAPI.Checker.Aux where
-
-import           Data.OpenApi.Internal
-
--- | Megre servers from parent (like PathItem) and child (like Operation)
-mergeServers :: [Server] -> [Server] -> [Server]
-mergeServers parent child = (error "FIXME: not implemented")
--- src/OpenAPI/Checker/Validate/Dereference.hs
+++
@@ -1,8 +0,0 @@
-module OpenAPI.Checker.Validate.Dereference where
-
-import           Data.OpenApi.Internal
-import           OpenAPI.Checker.Validate.Monad
-
--- | Throws error if param not found
-dereferenceParam :: Referenced Param -> TreeM t Param
-dereferenceParam = error "FIXME: dereferenceParam not implemented"
