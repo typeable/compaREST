d3667f51e1a4de72c1adad45beecbe3e7cdbed38
Aleksey Uymanov <s9gf4ult@gmail.com>
2021-04-06 18:17:57 +0500

    req body

--- src/OpenAPI/Checker/Validate/RequestBody.hs
+++ src/OpenAPI/Checker/Validate/RequestBody.hs
@@ -10,7 +10,8 @@ import Data.HList
 import Data.HashMap.Strict.InsOrd as IOHM
 import Data.Maybe
 import Data.OpenApi
-import Network.HTTP.Media
+import Data.Text (Text)
+import Network.HTTP.Media (MediaType, mainType, subType)
 import OpenAPI.Checker.Subtree
 import OpenAPI.Checker.Trace
 
@@ -31,16 +32,46 @@ instance Subtree RequestBody where
       case IOHM.lookup mediaType $ _requestBodyContent p of
         Nothing -> issueAt producer RequestMediaTypeNotFound
         Just prodMedia -> localStep (MediaTypeStep mediaType) $
-          checkCompatibility HNil (ProdCons prodMedia consMedia)
+          checkCompatibility (singletonH mediaType) (ProdCons prodMedia consMedia)
 
 
 instance Subtree MediaTypeObject where
-  type CheckEnv MediaTypeObject = '[]
+  type CheckEnv MediaTypeObject = '[MediaType]
   data CheckIssue MediaTypeObject
+    = MediaEncodingMissing
+    | MediaEncodingIncompat
     deriving (Eq, Ord, Show)
-  checkCompatibility _ (ProdCons p c) =
+  checkCompatibility env (ProdCons p c) = sequenceA_
+    [ tryCheckEncoding
+    , checkSchema ]
+    where
+      mediaType = getH @MediaType env
+      tryCheckEncoding =
+        if | "multipart" == mainType mediaType -> checkEncoding
+           | "application" == mainType mediaType &&
+             "x-www-form-urlencoded" == subType mediaType -> checkEncoding
+           | otherwise -> pure ()
+      checkEncoding = for_ (IOHM.toList $ _mediaTypeObjectEncoding c) $ \(paramName, consEncoding) ->
+        case IOHM.lookup paramName $ _mediaTypeObjectEncoding p of
+          Nothing -> issueAt producer MediaEncodingMissing
+          Just prodEncoding -> localStep (MediaTypeParamEncoding paramName)
+            $ checkCompatibility HNil
+            $ ProdCons prodEncoding consEncoding
+      checkSchema = (error "FIXME: check schema must be implemented later")
 
+instance Subtree Encoding where
+  type CheckEnv Encoding = '[]
+  data CheckIssue Encoding = EncodingNotSupported
+    --  FIXME: Support only JSON body for now. Then Encoding is checked only for
+    --  multipart/form-url-encoded
+    deriving (Eq, Ord, Show)
+  checkCompatibility _env _prodCons =
+    issueAt producer EncodingNotSupported
 
 instance Steppable RequestBody MediaTypeObject where
   data Step RequestBody MediaTypeObject = MediaTypeStep MediaType
+    deriving (Eq, Ord, Show)
+
+instance Steppable MediaTypeObject Encoding where
+  data Step MediaTypeObject Encoding = MediaTypeParamEncoding Text
     deriving (Eq, Ord, Show)
