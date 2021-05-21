44a3c27f4c5ce15f23910d0c49576e17b1fd32f7
iko <ilyakooo0@gmail.com>
2021-05-21 14:31:58 +0300

    Slightly better output signalling (#58)

--- src/OpenAPI/Checker/Run.hs
+++ src/OpenAPI/Checker/Run.hs
@@ -5,9 +5,10 @@ import qualified Data.ByteString.Char8 as BSC
 import Data.HList
 import qualified Data.Yaml as Yaml
 import OpenAPI.Checker.Options
-import OpenAPI.Checker.Subtree
 import OpenAPI.Checker.Paths
+import OpenAPI.Checker.Subtree
 import OpenAPI.Checker.Validate.OpenApi ()
+import System.Exit
 import Prelude hiding (id, (.))
 
 runChecker :: IO ()
@@ -19,12 +20,21 @@ runChecker = do
             Yaml.decodeFileEither path >>= \case
               Left yamlErr -> do
                 putStrLn "Could not parse as json or yaml"
-                putStrLn $ show jsonErr
-                putStrLn $ show yamlErr
+                print jsonErr
+                print yamlErr
                 fail "Exiting"
               Right s -> pure s
           Right s -> pure s
   a <- traced Root <$> parseSchema (clientFile opts)
   b <- traced Root <$> parseSchema (serverFile opts)
   let report = runCompatFormula $ checkCompatibility HNil Root (ProdCons a b)
-  BSC.putStrLn $ Yaml.encode report
+      output = case outputMode opts of
+        StdoutMode -> BSC.putStrLn
+        FileMode f -> BSC.writeFile f
+  case report of
+    Right () -> do
+      output "No breaking changes found ✨"
+      exitSuccess
+    Left e -> do
+      output $ Yaml.encode e
+      exitFailure
