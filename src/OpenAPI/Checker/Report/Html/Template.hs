1e6367ca9bb9187586db6cafbff82f74bd58c7ce
iko <ilyakooo0@gmail.com>
2021-06-21 19:52:22 +0300

    Embed styling

--- src/OpenAPI/Checker/Report/Html/Template.hs
+++ src/OpenAPI/Checker/Report/Html/Template.hs
@@ -1,10 +1,15 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module OpenAPI.Checker.Report.Html.Template
   ( template
   )
 where
 
 import Control.Monad.Identity
+import Data.ByteString (ByteString)
+import Data.FileEmbed
 import Data.Text (Text)
+import qualified Data.Text.Encoding as T
 import Text.DocTemplates
 
 template :: Template Text
@@ -13,13 +18,22 @@ template =
     "<!doctype html>\
     \<html lang=\"en\">\
     \<head>\
-    \<meta charset=\"utf-8\">\
-    \<title></title>\
-    \<meta name=\"description\" content=\"\">\
-    \<meta name=\"generator\" content=\"OpenApi Diff\" />\
-    \<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=yes\" />\
-    \</head>\
-    \<body>\
-    \$body$\
-    \</body>\
-    \</html>"
+    \<style>"
+      <> T.decodeUtf8 awsmCss
+      <> "</style>\
+         \<meta charset=\"utf-8\">\
+         \<title></title>\
+         \<meta name=\"description\" content=\"\">\
+         \<meta name=\"generator\" content=\"OpenApi Diff\" />\
+         \<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=yes\" />\
+         \</head>\
+         \<body>\
+         \<header><h1>⛄ OpenApi Diff</h1></header>\
+         \<main>\
+         \$body$\
+         \</main>\
+         \</body>\
+         \</html>"
+
+awsmCss :: ByteString
+awsmCss = $(makeRelativeToProject "awsm-css/dist/awsm.min.css" >>= embedFile)
